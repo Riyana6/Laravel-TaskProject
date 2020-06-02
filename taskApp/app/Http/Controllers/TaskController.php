9aa89726f540d7ef0aa69e246475d16c221a7dfe
JMJ Riyana <jeemariyana@gmail.com>
2020-06-02 21:24:23 +0530

    code

--- taskApp/app/Http/Controllers/TaskController.php
+++ taskApp/app/Http/Controllers/TaskController.php
@@ -7,24 +7,12 @@ use App\Task;
 
 class TaskController extends Controller
 {
-
     public function store(Request $request){
 
-
-
-
         $task=new Task;
-
-        $this->validate('$request',[
-            'task'=>'required|max:100|min:5',
-        
-        
-        ]);
-
         $task->task=$request->task;
         $task->save();
         return redirect()->back();
-
         //dd($request->all());
     
     }
