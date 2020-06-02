3150d580c63252b728563aae9116f95b39f8756d
JMJ Riyana <jeemariyana@gmail.com>
2020-06-02 16:30:45 +0530

    code

--- taskApp/resources/views/task.blade.php
+++ taskApp/resources/views/task.blade.php
@@ -12,10 +12,14 @@
     <h1>Daily Tasks</h1>
         <div class="row">
             <div class="col-md-12">
+
+                <form method="POST" action="/saveTask">
                 <input type="text" class="form-control" name="task" placeholder="Enter your task here"/>
                 </br>
-                <input type="button" class="btn- btn-primary" value="SAVE">
+                <input type="submit" class="btn- btn-primary" value="SAVE">
                 <input type="button" class="btn- btn-warning" value="CLEAR">
+                </form>
+
                 <table class="table table-dark">
                     <th>ID</th>
                     <th>Task</th>
