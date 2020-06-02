36188eeb9f26eae0c9f1d337b2b32cb8ac975f7a
JMJ Riyana <jeemariyana@gmail.com>
2020-06-02 21:06:39 +0530

    code

--- taskApp/app/Http/Controllers/TaskController.php
+++ taskApp/app/Http/Controllers/TaskController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class TaskController extends Controller
 {
     public function store(Request $request){
 
-        dd($request->all());
+        $task=new Task;
+        $task->task=$request->task;
+        $task->save();
+        return redirect()->back();
+        
+        //dd($request->all());
     
     }
 }
